003c0004d4ad8af5be01f477142d11239943a89c
kamiy2743 <kamiy2743@gmail.com>
2022-06-12 13:20:30 +0900

    プレイヤーを動かす

---
+++ Assets/_Project/0_Scripts/Player/PlayerMover.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MB
+{
+    public class PlayerMover : MonoBehaviour, IStaticAwake
+    {
+        [SerializeField] private float _moveSpeed;
+
+        private Rigidbody _rigidbody;
+
+        public void StaticAwake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            var moveVector = InputProvider.MoveVector();
+            _rigidbody.AddForce(moveVector * _moveSpeed);
+        }
+    }
+}
