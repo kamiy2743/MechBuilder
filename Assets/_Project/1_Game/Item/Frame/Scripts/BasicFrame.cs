4c7651bf6e846b8d3c648e459e00b7a56e17e167
kamiy2743 <kamiy2743@gmail.com>
2022-06-14 11:38:08 +0900

    ItemID追加

--- Assets/_Project/1_Game/Item/Frame/Scripts/BasicFrame.cs
+++ Assets/_Project/1_Game/Item/Frame/Scripts/BasicFrame.cs
@@ -6,6 +6,7 @@ namespace MB
 {
     public class BasicFrame : MonoBehaviour, IItem
     {
+        public int ID { get; private set; } = 5;
         public string Name { get; private set; } = "BasicFrame";
     }
 }
--- Assets/_Project/1_Game/Item/IItem.cs
+++ Assets/_Project/1_Game/Item/IItem.cs
@@ -2,6 +2,7 @@ namespace MB
 {
     public interface IItem
     {
+        int ID { get; }
         string Name { get; }
     }
 }
