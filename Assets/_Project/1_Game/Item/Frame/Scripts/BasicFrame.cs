3630595bb3e3c46ff2d04ce834b5d3c62c97b7a3
kamiy2743 <kamiy2743@gmail.com>
2022-06-14 13:03:27 +0900

    アイテムからメッシュとマテリアルの取得

--- Assets/_Project/1_Game/Item/Frame/Scripts/BasicFrame.cs
+++ Assets/_Project/1_Game/Item/Frame/Scripts/BasicFrame.cs
@@ -9,15 +9,16 @@ namespace MB
         public int ID { get; private set; } = 5;
         public string Name { get; private set; } = "BasicFrame";
 
-        public Mesh Mesh => _mr.material;
-        public Material Material => _rendererr.material;
+        public Mesh Mesh => _mf.mesh;
+        public Material Material => _mr.material;
 
         private MeshFilter _mf;
         private MeshRenderer _mr;
 
         void Awake()
         {
-            _mf = Getcom
+            _mf = this.gameObject.GetComponentInSelfOrChildren<MeshFilter>("meshfilter not found");
+            _mr = this.gameObject.GetComponentInSelfOrChildren<MeshRenderer>("meshrenderer not found");
         }
     }
 }
--- Assets/_Project/1_Game/Player/Raycast/ItemPlacer.cs
+++ Assets/_Project/1_Game/Player/Raycast/ItemPlacer.cs
@@ -20,27 +20,49 @@ namespace MB
 
         private void Preview(RaycastHit hit, IItem other)
         {
-            _lastItemID = other.ID;
+            if (_lastItemID != other.ID)
+            {
+                _lastItemID = other.ID;
+                _previewObject.CopyMeshAndMaterial(other);
+            }
 
             var normal = hit.normal;
             var point = hit.point;
             var otherRotation = hit.collider.transform.rotation;
 
-            _previewObject.transform.position = point + normal * 0.25f;
-            _previewObject.transform.rotation = otherRotation;
+            _previewObject.SetVisible(true);
+            _previewObject.Transform.position = point + normal * 0.25f;
+            _previewObject.Transform.rotation = otherRotation;
         }
 
         private class PreviewObject
         {
-            public Transform transform { get; private set; }
+            public Transform Transform { get; private set; }
+
+            private MeshFilter _mf;
+            private MeshRenderer _mr;
 
             public PreviewObject()
             {
                 var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 MonoBehaviour.Destroy(go.GetComponent<BoxCollider>());
 
-                transform = go.transform;
-                transform.localScale = Vector3.one * 0.5f;
+                Transform = go.transform;
+                _mf = go.GetComponent<MeshFilter>();
+                _mr = go.GetComponent<MeshRenderer>();
+
+                SetVisible(false);
+            }
+
+            public void SetVisible(bool value)
+            {
+                _mr.enabled = value;
+            }
+
+            public void CopyMeshAndMaterial(IItem other)
+            {
+                _mf.mesh = other.Mesh;
+                _mr.material = other.Material;
             }
         }
     }
