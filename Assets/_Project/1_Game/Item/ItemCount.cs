06d8ccdaa7db6d58b968189fbe53d38a2bbdb7da
kamiy2743 <kamiy2743@gmail.com>
2022-06-15 13:08:16 +0900

    アイテム数の値オブジェクト定義

---
+++ Assets/_Project/1_Game/Item/ItemCount.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ItemCount
+{
+    private static int _min = 0;
+
+    public int Value { get; }
+    private int _max;
+
+    public ItemCount(int value, int max)
+    {
+        if (max <= _min)
+        {
+            throw new System.Exception("最大数が最小数以下です");
+        }
+
+        _max = max;
+
+        if (value < _min)
+        {
+            throw new System.Exception("アイテム数が" + _min + "より小さいです");
+        }
+        if (value > max)
+        {
+            throw new System.Exception("アイテム数が" + _max + "より大きいです");
+        }
+
+        Value = value;
+    }
+
+    public ItemCount(int max)
+    {
+        _max = max;
+        Value = _min;
+    }
+
+    public ItemCount Add(ItemCount add)
+    {
+        return new ItemCount(Value + add.Value, _max);
+    }
+}
