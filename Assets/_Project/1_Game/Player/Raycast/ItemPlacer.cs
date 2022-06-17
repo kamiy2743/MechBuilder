c09303f12927d677686685b2207649471671d020
kamiy2743 <kamiy2743@gmail.com>
2022-06-17 13:43:42 +0900

    PreviewObjectクラスを別ファイルに分割

--- Assets/_Project/1_Game/Player/Raycast/ItemPlacer.cs
+++ Assets/_Project/1_Game/Player/Raycast/ItemPlacer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace MB
 {
-    public class ItemPlacer : IRaycasterStateProcessor
+    public partial class ItemPlacer : IRaycasterStateProcessor
     {
         public int LayerMask { get; private set; } = (1 << UnityEngine.LayerMask.NameToLayer("Item"));
 
@@ -36,40 +36,6 @@ namespace MB
             _previewObject.SetVisible(true);
             _previewObject.Transform.position = point + normal * (originalItemInhand.Collider.Size.x * other.Collider.Size.x);
             _previewObject.Transform.rotation = otherRotation;
-        }
-
-        private class PreviewObject
-        {
-            public Transform Transform { get; private set; }
-
-            private MeshFilter _mf;
-            private MeshRenderer _mr;
-
-            public PreviewObject()
-            {
-                var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                MonoBehaviour.Destroy(go.GetComponent<BoxCollider>());
-
-                Transform = go.transform;
-                _mf = go.GetComponent<MeshFilter>();
-                _mr = go.GetComponent<MeshRenderer>();
-
-                SetVisible(false);
-            }
-
-            public void SetVisible(bool value)
-            {
-                _mr.enabled = value;
-            }
-
-            public void CopyMeshAndMaterial(ItemID itemID)
-            {
-                var originalItem = OriginalFieldItems.Instance.GetOriginalData(itemID);
-
-                Transform.localScale = originalItem.Apperance.Scale;
-                _mf.mesh = originalItem.Apperance.MeshFilter.mesh;
-                _mr.material = originalItem.Apperance.MeshRenderer.material;
-            }
         }
     }
 }
---
+++ Assets/_Project/1_Game/Player/Raycast/PreviewObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MB
+{
+    public partial class ItemPlacer
+    {
+        private class PreviewObject
+        {
+            public Transform Transform { get; private set; }
+
+            private MeshFilter _mf;
+            private MeshRenderer _mr;
+
+            public PreviewObject()
+            {
+                var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                MonoBehaviour.Destroy(go.GetComponent<BoxCollider>());
+
+                Transform = go.transform;
+                _mf = go.GetComponent<MeshFilter>();
+                _mr = go.GetComponent<MeshRenderer>();
+
+                SetVisible(false);
+            }
+
+            public void SetVisible(bool value)
+            {
+                _mr.enabled = value;
+            }
+
+            public void CopyMeshAndMaterial(ItemID itemID)
+            {
+                var originalItem = OriginalFieldItems.Instance.GetOriginalData(itemID);
+
+                Transform.localScale = originalItem.Apperance.Scale;
+                _mf.mesh = originalItem.Apperance.MeshFilter.mesh;
+                _mr.material = originalItem.Apperance.MeshRenderer.material;
+            }
+        }
+    }
+}
