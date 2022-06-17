61f23bfc1c2f8d1b8fe1c43790e71c0001bf30d9
kamiy2743 <kamiy2743@gmail.com>
2022-06-17 10:18:31 +0900

    ItemIDにEqualsをGetHashCode追加

--- Assets/_Project/1_Game/Item/ItemID.cs
+++ Assets/_Project/1_Game/Item/ItemID.cs
@@ -45,5 +45,30 @@ namespace MB
         {
             return !(id1 == id2);
         }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var other = (ItemID)obj;
+            return _value == other._value;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
