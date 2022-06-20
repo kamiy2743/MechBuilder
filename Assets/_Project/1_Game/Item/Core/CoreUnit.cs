46139a1fb8c432cc7edeafc61dcdd395d8547c7a
kamiy2743 <kamiy2743@gmail.com>
2022-06-20 12:04:43 +0900

    wip CoreUnit

---
+++ Assets/_Project/1_Game/Item/Core/CoreUnit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MB
+{
+    public class CoreUnit : MonoBehaviour, IFieldItem
+    {
+        public ItemID ID { get; private set; }
+
+        public GameObject GameObject => this.gameObject;
+        public ItemCollider Collider { get; private set; }
+
+        public ItemApperance Apperance { get; private set; }
+
+        public void Initialize(ItemID itemID)
+        {
+            ID = itemID;
+
+            Collider = GetComponent<ItemCollider>();
+
+            Apperance = this.gameObject.GetComponentInSelfOrChildren<ItemApperance>();
+        }
+    }
+}
